779b9f8cdb94a3be9739d9ba0949717b77d2c970
dicpic <24DReshetnyk.bife@wicklowvec.ie>
2024-11-28 13:08:33 +0000

    dragging class and usable is done
    debugged stuff like multiple pick ups

--- scripts/objects/puzzles/rubbish.gd
+++ scripts/objects/puzzles/rubbish.gd
@@ -6,16 +6,22 @@ extends Sprite2D
 @export var amount: int = 5
 @export var id: int = -1
 var z_index_count = 0
+var id_count = 0
 
 
 func scatter(object):
 		object.frame = randi() % hframes
 		object.z_index = z_index_count
 		z_index_count+=1
+		object.id += id_count
+		id_count +=1
+		
 		object.position.x += (randf()-0.5) * child_offset
 		object.position.y += (randf()-0.5) * child_offset
+		
 		var obj_scale = randf_range(scale_lower, scale_upper)
 		object.scale = Vector2(obj_scale, obj_scale) 
+		
 		object.show()
 
 func init():
--- scripts/objects/puzzles/wires/dragging_2d.gd
+++ scripts/objects/puzzles/wires/dragging_2d.gd
@@ -10,9 +10,11 @@ signal picked
 var id: int = -1 
 
 var dragging = false  # Variable to track whether we are dragging
-static var currently_sprited = false
+#static var currently_sprited = false
+static var currently_sprited: Sprite2D = null
+#static var top_index = 0
 
-var offset = Vector2()  # Offset from the mouse to the sprite position when starting drag
+#var offset = Vector2()  # Offset from the mouse to the sprite position when starting drag
 
 func _ready() -> void:
 	get_id()
@@ -23,17 +25,17 @@ func get_id():
 		else:
 			id = -1	
 
-func clicked():
-	if Input.is_action_just_released("player_shoot"):
-		currently_sprited = false
-		print("dropped_", id)
-		sprite.z_index -= 1
-		dropped.emit(id)
-	elif Input.is_action_pressed("player_shoot"):
-		currently_sprited = true
-		print("picked_", id)
-		sprite.z_index += 1
-		picked.emit(id)
+#func clicked():
+	#if Input.is_action_just_released("player_shoot"):
+		#currently_sprited = false
+		#print("dropped_", id)
+		#sprite.z_index -= 1
+		#dropped.emit(id)
+	#elif Input.is_action_pressed("player_shoot"):
+		#currently_sprited = true
+		#print("picked_", id)
+		#sprite.z_index += 1
+		#picked.emit(id)
 
 #func _input(event):
 #		
@@ -63,28 +65,56 @@ func clicked():
 	#if event is InputEventMouseMotion and dragging:
 		#sprite.global_position = event.position + offset
 
+func is_mouse_over() -> bool:
+	# Check if the mouse is over the sprite
+	var mouse_pos = get_viewport().get_mouse_position()
+	return (mouse_pos.x > sprite.global_position.x - sprite.get_rect().size.x * sprite.scale.x / 2 and
+			mouse_pos.x < sprite.global_position.x + sprite.get_rect().size.x * sprite.scale.x / 2 and
+			mouse_pos.y > sprite.global_position.y - sprite.get_rect().size.y * sprite.scale.y / 2 and
+			mouse_pos.y < sprite.global_position.y + sprite.get_rect().size.y * sprite.scale.y / 2)
 
-#func is_mouse_over() -> bool:
-	## Check if the mouse is over the sprite
-	#var mouse_pos = get_viewport().get_mouse_position()
-	#return (mouse_pos.x > sprite.global_position.x - sprite.get_rect().size.x * sprite.scale.x / 2 and
-			#mouse_pos.x < sprite.global_position.x + sprite.get_rect().size.x * sprite.scale.x / 2 and
-			#mouse_pos.y > sprite.global_position.y - sprite.get_rect().size.y * sprite.scale.y / 2 and
-			#mouse_pos.y < sprite.global_position.y + sprite.get_rect().size.y * sprite.scale.y / 2)
+#func top_check():
+	#if sprite.z_index > top_index:
+		#top_index = sprite.z_index
+	#return top_index <= sprite.z_index
 
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and not event.pressed and dragging:
+		currently_sprited = null
+		#sprite.z_index -= 1
+		print("dropped_", id)
+		dropped.emit(id)
+	
+	if currently_sprited == sprite and event is InputEventMouseMotion and dragging:
+		sprite.global_position += event.relative
 
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	print("input_", viewport)
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-		
-		if currently_sprited:
-			sprite.global_position = event.position + offset
+	if event is InputEventMouseButton and event.pressed and dragging:
+		if currently_sprited == null and is_mouse_over():
+			currently_sprited = sprite
+			#sprite.z_index += 1
+			print("picked_", id)
+			picked.emit(id)		
+
+	#if event is InputEventMouseButton:
+		#if event.button_index == MOUSE_BUTTON_LEFT:
+			#clicked()
+			##offset = sprite.global_position - event.position  # Calculate offset
+	#
+	#if currently_sprited:
+		#print("Calculating: gl_ps = ", sprite.global_position, ", ev_ps = ", event.position, ", ofst = ", offset)
+		#sprite.global_position = get_viewport().get_mouse_position()
+		##sprite.global_position = event.position + offset
+		#print("Calculated: gl_ps = ", sprite.global_position)
 
 
 func _on_mouse_entered() -> void:
-	print("mouse_entered")
-	dragging = true
+	if !dragging and !currently_sprited:
+		print("mouse_entered")
+		dragging = true
 
 func _on_mouse_exited() -> void:
-	print("mouse_exited")
-	dragging = false
+	if !is_mouse_over() and dragging and !currently_sprited:
+		print("mouse_exited")
+		dragging = false
