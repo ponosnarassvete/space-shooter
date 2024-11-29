71e1d99ea228779ac1956f15c123ef86f8a0652a
dicpic <24DReshetnyk.bife@wicklowvec.ie>
2024-11-29 12:35:34 +0000

    tool using WORKS LETS GOOO

--- scripts/character/player/tools/tool_manager.gd
+++ scripts/character/player/tools/tool_manager.gd
@@ -1,6 +1,11 @@
 class_name Tool_Manager
 extends Node
 
+signal tool_used
+signal tool_swapped
+signal tool_added
+signal tool_removed
+
 @export var tool_use_area: Area2D
 var active_tool: String = "Grabber"
 var tool_bar: Array = [] # Grabber
@@ -8,23 +13,26 @@ var tool_bar: Array = [] # Grabber
 						 # Hacker
 						 # Attacker
 
-func _ready() -> void:
-	tool_use_area = $Tool_Use_Area
-
 func _input(event: InputEvent) -> void:
 	
 	if Input.is_action_pressed("tool_swap_up"):
 		tool_bar.insert(0, active_tool)
 		active_tool = tool_bar.pop_back()
+		tool_swapped.emit()
 	elif Input.is_action_pressed("tool_swap_down"):
 		tool_bar.append(active_tool)
 		active_tool = tool_bar.pop_front()
+		tool_swapped.emit()
+	
+	if Input.is_action_just_pressed("player_shoot"):
+		var mouse_pos = get_viewport().get_mouse_position()
+		if tool_use_area.get_viewport_rect().has_point(mouse_pos):
+			tool_used.emit()
+			print(active_tool)
 
-func used():
-	if Input.is_action_pressed("player_shoot"):
-		pass
-		
 
+func tool_add(tool: String):
+	pass
 
-func _on_tool_use_area_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	pass # Replace with function body.
+func tool_remove(tool: String):
+	pass
