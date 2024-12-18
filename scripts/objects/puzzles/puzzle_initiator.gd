a2edd4a41bf0398dab70ebbb5bb5171edfd47d1f
dicpic <24DReshetnyk.bife@wicklowvec.ie>
2024-12-18 04:00:59 +0000

    saves done, everything is ready for level designs I guess

--- scripts/objects/puzzles/puzzle_initiator.gd
+++ scripts/objects/puzzles/puzzle_initiator.gd
@@ -6,6 +6,8 @@ signal sesame
 var puzzle = preload("res://scenes/objects/puzzle_test.tscn")
 var initiated: bool = false
 var sesame_in_action: bool = false
+var puzzles = {}
+var current_puzzle_id: int = -1
 @export var doors: Node
 
 func _ready() -> void:
@@ -19,18 +21,26 @@ func activated(id: int = -1):
 		sesame_in_action = true
 		
 		if !initiated:
-			create(id)
+			current_puzzle_id = id
+			if !puzzles.has(id):
+				create(id)
+			else:
+				add_child(puzzles[id])
 			await doors.finished
 		else:
 			GameStateService.save_game_state(GSSConst.SAVE_FILE)
 			await doors.finished
-			self.get_child(1).queue_free()
+			print(puzzles)
+			remove_child(puzzles[current_puzzle_id])
+			#self.get_child(1).queue_free()
+			
 		
 		sesame_in_action = false
 		initiated = !initiated
 
 func create(id: int = -1):
-	var puzzle_instance = puzzle.instantiate()
-	puzzle_instance.name = "puzzle_" + str(id)
+	var puzzle_instance = puzzle.instantiate(1)
+	puzzles.merge({id: puzzle_instance})
+	puzzle_instance.name = "puzzle" + str(id)
 	self.add_child(puzzle_instance)
 	puzzle_instance.puzzle_init()
